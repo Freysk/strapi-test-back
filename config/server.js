dbea7d534b781ec57fad89d96dc839ec0b42eec0
Frédéric Defays <justforenroll@gmail.com>
2022-02-10 14:39:48 +0100

    :tada: first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '13edc60539601a71b764629646dbd42e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3f24db47-b12a-4616-82e6-9180756e6a83'
+};
